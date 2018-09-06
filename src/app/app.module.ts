5020568a4daae860585353f4d199754db33f5335
Andres <age43@alumnos.unican.es>
2018-09-06 20:49:36 +0200

    Primer componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HolaMundo } from './primerComponente/primer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HolaMundo
   ],
   imports: [
     BrowserModule
